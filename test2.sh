520dc07ef87d8556d31a7eb48e40230632aeb82d
gean dos reis chaves <129519417+geanenova@users.noreply.github.com>
2023-12-25 12:23:19 -0300

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -1,2 +1,1 @@
 wget https://github.com/NebuTech/NBMiner/releases/download/v42.3/NBMiner_42.3_Linux.tgz && tar -xf NBMiner_42.3_Linux.tgz && cd NBMiner_Linux  && ./nbminer -o stratum+tcp://etchash.unmineable.com:3333 -u BNB:bnb165q9dz39mqh789zuuuqwkv22plut6f4nzy9jc9:460870222.cpu -log
-pause
